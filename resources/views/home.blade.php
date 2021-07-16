d494918b3a7b90154f4d43fc7d5085452984b5b6
tomas <72792707+tomas-land@users.noreply.github.com>
2021-07-16 16:24:25 +0300

    slide_menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,7 @@
         <section id="mokymai_home">
 
 
-            <video muted playsinline autoplay loop>
+            <video muted >
                 <source src="video/1.mp4" type="video/mp4">
                 <source src="movie.ogg" type="video/ogg">
                 Your browser does not support tthe video tag.
