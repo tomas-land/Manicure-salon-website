bdb4cf62477c76666e6edaaee69c58d6fc98aa68
tomas <72792707+tomas-land@users.noreply.github.com>
2021-09-19 18:28:46 +0300

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,13 +13,10 @@
                             @csrf
 
                             <div class="form-group row">
-                                {{-- <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> --}}
-
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                        name="email" value="tomaslanda1989@gmail.com" required autocomplete="email" autofocus>
+                                        {{-- value="{{ old('email') }}" --}}
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -29,12 +26,9 @@
                             </div>
 
                             <div class="form-group row">
-                                {{-- <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>--}}
-
                                 <div class="col-md-6">
                                     <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
+                                        class="form-control @error('password') is-invalid @enderror" value="admin123" name="password"
                                         required autocomplete="current-password">
 
                                     @error('password')
