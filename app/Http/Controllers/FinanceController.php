5dc601383ee6a210cb9c715e02bd861e726224cb
tomas <72792707+tomas-land@users.noreply.github.com>
2021-12-02 00:02:38 +0200

    added counting of total income for the last three months

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Charts\MonthlyIncomeChart;
 use App\Models\Visit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
@@ -38,71 +37,110 @@ class FinanceController extends Controller
 //send date and use $request->date /fetch by month
 
         // count total sum of specific month
-        function countTotal($month)
-        {
-            $monthTotal = 0;
-            foreach ($month as $key => $value) {
-                $monthTotal += $value;
+       
+        
+        function assignPricesToDate($visits){
+            $array=[];
+            if(!empty($visits)){
+            foreach ($visits as $date => $price) {
+                $newDate = Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d'); // date format change
+                $array[$newDate][] = $price; // adding prices to array to corresponding date
             }
-            return $month = $monthTotal;
+     
+        } else {
+                dd('ok');
+            }
+               return $array;
         }
-        //--------------------------------------december
-        $startDate1 = '2021-12-01';
-        $endDate1 = '2021-12-31';
-        $gruodis = Visit::
-            whereBetween('start', [$startDate1, $endDate1])
-            ->pluck('price', 'start');
 
-        $gruodisTotal = countTotal($gruodis);
-        //--------------------------------------january
-        $startDate2 = '2022-01-01';
-        $endDate2 = '2022-01-31';
-        $sausis = Visit::
-            whereBetween('start', [$startDate2, $endDate2])
-            ->pluck('price', 'start');
-        $sausisTotal = countTotal($sausis);
+        function sumPricesAssignToDate($array){
+            $visits=[];
+            foreach ($array as $k => $v) { // looping through array with dates  and suming prices to corresponding date
+                $visits[$k] = array_sum($v);
+            }
+            return $visits;
+        }
+
+        function countTotal($visits)
+        {
+            $visitsIncomeTotal = 0;
+            foreach ($visits as $date => $price) {
+                $visitsIncomeTotal += $price;
+            }
+            return $visitsIncomeTotal;
+        }
+
+
+        //--------------------------------------december
+        // $startDate1 = '2021-12-01';
+        // $endDate1 = '2021-12-31';
+        // $gruodis = Visit::
+        //     whereBetween('start', [$startDate1, $endDate1])
+        //     ->pluck('price', 'start');
+       
+        // $gruodisTotal = countTotal($gruodis);
+        // //--------------------------------------january
+        // $startDate2 = '2022-01-01';
+        // $endDate2 = '2022-01-31';
+        // $sausis = Visit::
+        //     whereBetween('start', [$startDate2, $endDate2])
+        //     ->pluck('price', 'start');
+        // $sausisTotal = countTotal($sausis);
         //------------------------------------------------------------------------------------------------------------------
+        $now = Carbon::now();
         $today = Carbon::today();
         $days30back = Carbon::today()->subDays(12); // 31 days back from today
-        $beginingOfMonth = Carbon::today('Europe/Vilnius')->startOfMonth();
+        $beginingOfCurrentMonth = Carbon::now('Europe/Vilnius')->startOfMonth();
         $endingOfMonth = Carbon::today('Europe/Vilnius')->endOfMonth();
+        $monthBeforeBegining = Carbon::now()->subMonth('1')->startOfMonth();
+        $monthBeforeEnding = Carbon::now()->subMonth('1')->endOfMonth();
+        $twoMonthsBeforeBegining = Carbon::now()->subMonth('2')->startOfMonth();
+        $twoMonthsBeforeEnding = Carbon::now()->subMonth('2')->endOfMonth();
+    
         $visits1 = Visit::where('start', $today)->pluck('price', 'start')->first(); // null
         $visits2 = Visit::whereDate('start', '<=', Carbon::now())->pluck('price', 'start'); // array{"2021-11-23 19:37:00" => 146} takes visits from now to past time
         $visits3 = Visit::select(DB::raw('start')) // returns array of objects with time
             ->whereDate('start', '<=', Carbon::now())
             ->get();
         //------------------------------------------------------------------------------------------------------------------
-        $visitsOfcurrentMonth = Visit::whereBetween('start', [$beginingOfMonth, $endingOfMonth])
-            ->pluck('price', 'start');
+        $visitsOfcurrentMonth = Visit::whereBetween('start', [$beginingOfCurrentMonth, $now])
+        ->pluck('price', 'start');
+        $assignedPrices = assignPricesToDate($visitsOfcurrentMonth);
+        $summedUpPrices = sumPricesAssignToDate($assignedPrices);
+        $currentMonthTotal = countTotal($summedUpPrices);
+        
+        $visitsOfMonthBefore = Visit::whereBetween('start', [$monthBeforeBegining, $monthBeforeEnding])
+        ->pluck('price', 'start');
+        $assignedPrices2 = assignPricesToDate($visitsOfMonthBefore);
+        $summedUpPrices2 = sumPricesAssignToDate($assignedPrices2);
+        $monthBeforeTotal = countTotal($summedUpPrices2);
 
-        foreach ($visitsOfcurrentMonth as $date => $price) {
-            $newDate = Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d'); // date format change
-            $array[$newDate][] = $price; // adding prices to array to corresponding date
-        }
-        foreach ($array as $k => $v) { // looping through array with dates  and suming prices to corresponding date
-            $array2[$k] = array_sum($v);
-        }
-
-        $chart = new MonthlyIncomeChart;
-        $chart->labels(array_keys($array2));
+        $visitsOfTwoMonthsBefore = Visit::whereBetween('start', [$twoMonthsBeforeBegining, $twoMonthsBeforeEnding])
+        ->pluck('price', 'start');
+        $assignedPrices3 = assignPricesToDate($visitsOfTwoMonthsBefore);
+        $summedUpPrices3 = sumPricesAssignToDate($assignedPrices3);
+        $twoMonthsBeforeTotal = countTotal($summedUpPrices3);
+       
+        Carbon::setLocale('LT');
+//  dd($visits);
+        //  Laravel Charts -----------------------------------------------------------------------------------------------------------
+        // $chart = new MonthlyIncomeChart;
+        // $chart->labels(array_keys($array2));
         // $chart->height(600);
         // $chart->minimalist(true);
         // $chart->title('diagrama',30 ..);
-        $chart->displaylegend(false);
-        $chart->dataset('My dataset', 'bar', array_values($array2))
-            // ->fontcolor("red")
-            // ->backgroundcolor("rgb(200, 99, 132)")
-            ->fill(true);
-            // ->linetension(0.5)
-            // ->dashed([10]);
-
-// $chart->barWidth(9.99);
-        // 
+        // $chart->displaylegend(false);
+        // $chart->dataset('My dataset', 'bar', array_values($array2))
+        // ->fontcolor("red")
+        // ->backgroundcolor("rgb(200, 99, 132)")
+        // ->fill(true);
+        // ->linetension(0.5)
+        // ->dashed([10]);
+        // $chart->barWidth(9.99);
+        //
         // // $chart->minimalist(true);
         // $chart->displayAxes(true);
-
-        return view('admin.finance.index', compact('gruodisTotal', 'sausisTotal', 'chart','array2'));
-
+        return view('admin.finance.index', compact('summedUpPrices','currentMonthTotal','monthBeforeTotal','twoMonthsBeforeTotal'));
     }
 
     /**
--- resources/views/admin/finance/index.blade.php
+++ resources/views/admin/finance/index.blade.php
@@ -2,45 +2,36 @@
 @section('admin-content')
 
     <div id="finance">
-
         {{-- {!! $chart->container() !!}
             {!! $chart->script() !!} --}}
         <div class="chart">
             <canvas id="myChart" width="800" height="200"></canvas>
         </div>
         <script>
-            var visits = {!! json_encode($array2) !!};
-
+            var currentMonthVisits = {!! json_encode($summedUpPrices) !!};
             const ctx = document.getElementById('myChart').getContext('2d');
-            // let minDataValue = Math.min(mostNegativeValue, options.suggestedMin);
-            // let maxDataValue = Math.max(mostPositiveValue, options.suggestedMax);
-            // Chart.defaults.global.defaultColor = 'rgba(55, 199, 132)';
-            // Chart.defaults.global.defaultFontSize = 15;
-            // Chart.defaults.global.defaultFontColor = '#a95da9';
             Chart.defaults.color = '#a385b1';
             const myChart = new Chart(ctx, {
-
                 type: 'bar',
                 data: {
-                    labels: Object.keys(visits),
+                    labels: Object.keys(currentMonthVisits),
                     datasets: [{
                         label: 'Dienos pajamos',
-                        data: Object.values(visits),
+                        data: Object.values(currentMonthVisits),
                         // backgroundColor: 'rgba(130, 61, 130, 0.5)',
                         // borderColor: 'rgba(255, 99, 132, 0.1)',
                         // borderWidth: 3,
                         // color: 'rgba(255, 99, 132, 0,1)'
-
+                        // barThickness: 0.9
+                        // barPercentage:0.5
+                        maxBarThickness: 30
                     }]
                 },
                 //------------------------------
                 options: {
                     elements: {
                         bar: {
-
-                            // backgroundColor:'green'
-                            barPercentage: 0.1,
-                            hoverBackgroundColor: '#964d96'
+                            hoverBackgroundColor: '#964d96',
                         }
                     },
                     plugins: {
@@ -71,10 +62,8 @@
                 }
             });
         </script>
-        {{-- </div> --}}
 
         <div class="card">
-            {{-- <a class="add-btn" href="{{ route('clients.create') }}">Pridėti naują klientą</a> --}}
             <table>
                 <tr>
                     <th>Metai</th>
@@ -82,18 +71,25 @@
                     <th>Pajamos</th>
                 </tr>
                 <tr>
-                    <td>2022</td>
-                    <td>Sausis</td>
-                    <td>{{ $sausisTotal }}</td>
+                    <td>{{ \Carbon\Carbon::now()->format('Y') }}</td>
+                    <td>{{ ucwords(\Carbon\Carbon::now()->translatedFormat('F')) }}</td>
+                    <td>{{ $currentMonthTotal }}</td>
                 </tr>
                 <tr>
-                    <td>2021</td>
-                    <td>Gruodis</td>
-                    <td>{{ $gruodisTotal }}</td>
+                    <td>{{ \Carbon\Carbon::now()->format('Y') }}</td>
+                    <td>{{ ucwords(
+                        \Carbon\Carbon::now()->subMonth()->translatedFormat('F'),
+                    ) }}
+                    </td>
+                    <td>{{ $monthBeforeTotal }}</td>
                 </tr>
                 <tr>
-                    <td>2021</td>
-                    <td>Lapkritis</td>
+                    <td>{{ \Carbon\Carbon::now()->format('Y') }}</td>
+                    <td>{{ ucwords(
+                        \Carbon\Carbon::now()->subMonth('2')->translatedFormat('F'),
+                    ) }}
+                    </td>
+                    <td>{{ $twoMonthsBeforeTotal }}</td>
 
                 </tr>
 
