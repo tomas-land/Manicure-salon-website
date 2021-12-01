e6ed280249f63c45ef6b364d92e633a1bb5c20f2
tomas <72792707+tomas-land@users.noreply.github.com>
2021-12-02 00:13:48 +0200

    little clean-up

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Visit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 
 class FinanceController extends Controller
 {
@@ -16,45 +15,20 @@ class FinanceController extends Controller
      */
     public function index(Request $request)
     {
-        // $services = SubService::all()->toArray();
-        // $randomService=Arr::random($services);
-        // $visits = Visit::
-        //         $lapkritis = Visit::whereDate('start','Y-m-d')->get();
-        // dd($lapkritis);
-        // $months = Visit::orderBy('start','DESC')->where('start',Carbon->distinct()->pluck('price','start')->;
-        // $date = '2021-11-23 19:37:00';
-        // $months=Carbon::createFromFormat('Y-m-d H:i:s', $request->start)->format('Y-m-d');
-        // dd($months);
-        // $gruodis= Visit::where('start','2021-11')->pluck('start','price');
-        // dd($gruodis);
 
-        // $currentMonth = Visit::select('start')->whereMonth('start',Carbon::now()->month)
-        // ->whereYear('start', Carbon::now()->year)
-        // ->sum("price");
-
-// dd($currentMonth);
-
-//send date and use $request->date /fetch by month
-
-        // count total sum of specific month
-       
-        
-        function assignPricesToDate($visits){
-            $array=[];
-            if(!empty($visits)){
+        function assignPricesToDate($visits)
+        {
+            $array = [];
             foreach ($visits as $date => $price) {
                 $newDate = Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y-m-d'); // date format change
                 $array[$newDate][] = $price; // adding prices to array to corresponding date
             }
-     
-        } else {
-                dd('ok');
-            }
-               return $array;
+            return $array;
         }
 
-        function sumPricesAssignToDate($array){
-            $visits=[];
+        function sumPricesAssignToDate($array)
+        {
+            $visits = [];
             foreach ($array as $k => $v) { // looping through array with dates  and suming prices to corresponding date
                 $visits[$k] = array_sum($v);
             }
@@ -70,22 +44,6 @@ class FinanceController extends Controller
             return $visitsIncomeTotal;
         }
 
-
-        //--------------------------------------december
-        // $startDate1 = '2021-12-01';
-        // $endDate1 = '2021-12-31';
-        // $gruodis = Visit::
-        //     whereBetween('start', [$startDate1, $endDate1])
-        //     ->pluck('price', 'start');
-       
-        // $gruodisTotal = countTotal($gruodis);
-        // //--------------------------------------january
-        // $startDate2 = '2022-01-01';
-        // $endDate2 = '2022-01-31';
-        // $sausis = Visit::
-        //     whereBetween('start', [$startDate2, $endDate2])
-        //     ->pluck('price', 'start');
-        // $sausisTotal = countTotal($sausis);
         //------------------------------------------------------------------------------------------------------------------
         $now = Carbon::now();
         $today = Carbon::today();
@@ -96,33 +54,34 @@ class FinanceController extends Controller
         $monthBeforeEnding = Carbon::now()->subMonth('1')->endOfMonth();
         $twoMonthsBeforeBegining = Carbon::now()->subMonth('2')->startOfMonth();
         $twoMonthsBeforeEnding = Carbon::now()->subMonth('2')->endOfMonth();
-    
-        $visits1 = Visit::where('start', $today)->pluck('price', 'start')->first(); // null
-        $visits2 = Visit::whereDate('start', '<=', Carbon::now())->pluck('price', 'start'); // array{"2021-11-23 19:37:00" => 146} takes visits from now to past time
-        $visits3 = Visit::select(DB::raw('start')) // returns array of objects with time
-            ->whereDate('start', '<=', Carbon::now())
-            ->get();
+
+        // $visits1 = Visit::where('start', $today)->pluck('price', 'start')->first(); // null
+        // $visits2 = Visit::whereDate('start', '<=', Carbon::now())->pluck('price', 'start'); // array{"2021-11-23 19:37:00" => 146} takes visits from now to past time
+        // $visits3 = Visit::select(DB::raw('start')) // returns array of objects with time
+        //     ->whereDate('start', '<=', Carbon::now())
+        //     ->get();
         //------------------------------------------------------------------------------------------------------------------
         $visitsOfcurrentMonth = Visit::whereBetween('start', [$beginingOfCurrentMonth, $now])
-        ->pluck('price', 'start');
+            ->pluck('price', 'start');
         $assignedPrices = assignPricesToDate($visitsOfcurrentMonth);
         $summedUpPrices = sumPricesAssignToDate($assignedPrices);
         $currentMonthTotal = countTotal($summedUpPrices);
-        
+
         $visitsOfMonthBefore = Visit::whereBetween('start', [$monthBeforeBegining, $monthBeforeEnding])
-        ->pluck('price', 'start');
+            ->pluck('price', 'start');
         $assignedPrices2 = assignPricesToDate($visitsOfMonthBefore);
         $summedUpPrices2 = sumPricesAssignToDate($assignedPrices2);
         $monthBeforeTotal = countTotal($summedUpPrices2);
 
         $visitsOfTwoMonthsBefore = Visit::whereBetween('start', [$twoMonthsBeforeBegining, $twoMonthsBeforeEnding])
-        ->pluck('price', 'start');
+            ->pluck('price', 'start');
         $assignedPrices3 = assignPricesToDate($visitsOfTwoMonthsBefore);
         $summedUpPrices3 = sumPricesAssignToDate($assignedPrices3);
         $twoMonthsBeforeTotal = countTotal($summedUpPrices3);
-       
-        Carbon::setLocale('LT');
-//  dd($visits);
+
+        Carbon::setLocale('LT'); // change language for carbon dates
+        return view('admin.finance.index', compact('summedUpPrices', 'currentMonthTotal', 'monthBeforeTotal', 'twoMonthsBeforeTotal'));
+        
         //  Laravel Charts -----------------------------------------------------------------------------------------------------------
         // $chart = new MonthlyIncomeChart;
         // $chart->labels(array_keys($array2));
@@ -140,72 +99,7 @@ class FinanceController extends Controller
         //
         // // $chart->minimalist(true);
         // $chart->displayAxes(true);
-        return view('admin.finance.index', compact('summedUpPrices','currentMonthTotal','monthBeforeTotal','twoMonthsBeforeTotal'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
