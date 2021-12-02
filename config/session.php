43e7ddec16908ee2475300c2b4dd24d7937a1159
tomas <72792707+tomas-land@users.noreply.github.com>
2021-12-02 20:49:36 +0200

    extended session time

--- config/session.php
+++ config/session.php
@@ -31,10 +31,10 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 1),
 
-    'expire_on_close' => false,
-
+    'expire_on_close' => true,
+    
     /*
     |--------------------------------------------------------------------------
     | Session Encryption
